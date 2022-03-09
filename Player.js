17ad9ad3fce216be484dbe390e8c53c98755981c
DrGoldenFlap <inmonmike@gmail.com>
2022-03-09 18:18:52 -0500

    Updated code

--- Player.js
+++ Player.js
@@ -1,1 +1,24 @@
-console.log("Hello World!")
+class Player {
+    constructor(name){
+        this.name = name;
+        this.win = 0;
+        this.getsturInput = "";
+        this.getsturList = ["Rock", "Paper", "Scissors", "Lizards", "Spock"];         
+    }
+    
+}
+class Computer extends Player {
+    constructor(name){
+        super(name);
+        }
+    chooseGestur(){
+       this.getsturInput = this.getsturList[Math.floor((Math.random() *this.getsturList.length))];
+    }
+}
+
+class Human extends Player{
+    constructor(name){
+        super(name);
+    }
+    this.getsturInput = this.getsturList
+}
